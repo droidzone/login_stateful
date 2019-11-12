299dee58c0cbdbd27fce5b54f5bb305af21ae06c
Droidzone (Joel G Mathew) <dz@droidzone.in>
2019-11-12 18:03:28 +0530

    First commit

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Log me in',
+      home: Scaffold(
+        body: Text('SHow a form here.'),
+      ),
+
+    );
+  }
+}
