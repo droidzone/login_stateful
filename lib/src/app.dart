c2022b0df05424e985f4dd2da762cbd95ad94fdd
joel@eyrie.in <joel@eyrie.in>
2019-11-12 22:16:48 +0530

    Stateful widget boiletplate

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Log me in',
+      home: Scaffold(
+        body: Text('Show a form here'),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState  extends State<LoginScreen> {
+  Widget @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
